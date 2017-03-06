a5f35eb38de7bc497cec49fd76c24d997bb15ffb
Scott Travis <scott@swt83.com>
2017-03-06 17:28:33 +0000

    Working.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # zip2city
 
+A PHP library for converting zip codes to city and state.
+
+Uses the Google Maps API w/out the need for an API key.
+
+## Install
+
+Normal install via composer.
+
+## Usage
+
+```php
+$response = Travis\Zip2City::run(22202);
+```
+
+Will return an array like this:
+
+```
+Array
+(
+    [zip] => Array
+        (
+            [long] => 22202
+            [short] => 22202
+        )
+
+    [city] => Array
+        (
+            [long] => Arlington
+            [short] => Arlington
+        )
+
+    [state] => Array
+        (
+            [long] => Virginia
+            [short] => VA
+        )
+
+)
+```
+
+This is designed for addresses in the United States.
--- src/Zip2City.php
+++ src/Zip2City.php
@@ -4,6 +4,14 @@ namespace Travis;
 
 class Zip2City
 {
+	public static $map = [
+
+		'city' => 'locality',
+		'state' => 'administrative_area_level_1',
+		'zip' => 'postal_code',
+
+	];
+
 	public static function run($zip)
 	{
 		// catch error...
@@ -12,8 +20,29 @@ class Zip2City
 		// get json response
 		$response = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.$zip);
 
+		// decode
+		$response = json_decode($response);
+
+		// convert to array
+		$response = object2array($response);
+
+		// init
+		$final = [];
+
+		// extract
+		foreach (ex($response, 'results.0.address_components', []) as $part)
+		{
+			foreach (static::$map as $key => $value)
+			{
+				if (in_array($value, $part['types']))
+				{
+					$final[$key]['long'] = $part['long_name'];
+					$final[$key]['short'] = $part['short_name'];
+				}
+			}
+		}
 
 		// return
-		return $response;
+		return $final;
 	}
 }
